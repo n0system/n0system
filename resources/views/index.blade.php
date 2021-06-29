0643934d9943a3417997c339a31c21b89230b286
Jonathan LUXIN <jo.luxin@gmail.com>
2021-06-29 11:37:34 +0200

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,32 @@
+<x-app-layout>
+    <div class="flex space-x-6 filters">
+        <div class="w-1/3">
+            <select name="category" id="category" class="w-full px-4 py-2 border-none rounded-xl">
+                <option value="Category 1">Category 1</option>
+                <option value="Category 2">Category 2</option>
+                <option value="Category 3">Category 3</option>
+                <option value="Category 4">Category 4</option>
+            </select>
+        </div>
+        <div class="w-1/3">
+            <select name="other_filters" id="other_filters" class="w-full px-4 py-2 border-none rounded-xl">
+                <option value="Filtre One">Filtre 1</option>
+                <option value="Filtre two">Filtre 2</option>
+                <option value="Filtre 3">Filtre 3</option>
+                <option value="Filtre 4">Filtre 4</option>
+            </select>
+        </div>
+        <div class="relative w-2/3">
+            <input type="search" name="" id="" placeholder="Trouver une idée"
+                class="w-full px-4 py-2 pl-8 placeholder-gray-900 bg-white border-none rounded-xl" />
+            <div class="absolute top-0 flex items-center h-full ml-2">
+                <svg class="w-4 text-gray-700" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                </svg>
+            </div>
+
+        </div>
+
+    </div> <!-- fin des filtres -->
+</x-app-layout>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
+
+<body class="font-sans text-sm text-gray-900 text bg-gray-background">
+    <header class="flex items-center justify-between px-8 py-4">
+        <a href="#"><img src="{{ asset('img/logo.svg') }}" alt="logo"></a>
+        <div class="flex items-center">
+            @if (Route::has('login'))
+                <div class="px-6 py-4">
+                    @auth
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <a href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                                                                                                                                                 this.closest('form').submit();">
+                                {{ __('Log Out') }}
+                            </a>
+                        </form>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+            <a href="#">
+                <img src="https://i.pravatar.cc/150?u=a042581f4e29026704d" alt="avatar" class="w-10 h-10 rounded-full ">
+            </a>
+        </div>
+
+    </header>
+    <main class="container flex mx-auto max-w-custom">
+        <div class="mr-5 w-70">
+            Ajout une idée. </div>
+
+        <div class="w-175">
+            <nav class="flex items-center justify-between text-xs">
+                <ul class="flex pb-3 space-x-10 font-semibold uppercase border-b-4">
+                    <li><a href="#" class="pb-3 border-b-4 border-blue">Toutes les idées(87)</a></li>
+                    <li><a href="#"
+                            class="pb-3 text-gray-400 transition duration-150 ease-in border-b-4 hover:border-blue">
+                            examiné(7)
+                        </a>
+                    </li>
+                    <li><a href="#"
+                            class="pb-3 text-gray-400 transition duration-150 ease-in border-b-4 hover:border-blue">
+                            En Traitement(1)
+                        </a>
+                    </li>
+                </ul>
+                <ul class="flex pb-3 space-x-10 font-semibold uppercase border-b-4">
+                    <li><a href="#"
+                            class="pb-3 text-gray-400 transition duration-150 ease-in border-b-4 hover:border-blue">
+                            Implementé(10)
+                        </a>
+                    </li>
+                    <li><a href="#"
+                            class="pb-3 text-gray-400 transition duration-150 ease-in border-b-4 hover:border-blue">
+                            Cloturé(54)
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="mt-8">
+                {{ $slot }}
+            </div>
+
+
+        </div>
+    </main>
+</body>
+
+</html>

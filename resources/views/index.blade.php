cd0a08b6286c177f7cd4fe16b863b7d836d348b9
Jonathan LUXIN <jo.luxin@gmail.com>
2021-06-29 16:12:20 +0200

    design idea card

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,4 +29,78 @@
         </div>
 
     </div> <!-- fin des filtres -->
+    <div class="my-6 space-y-6 ideas-container">
+        <div
+            class="flex transition duration-150 ease-in bg-white cursor-pointer idea-container rounded-xl hover:shadow-card ">
+            <div class="px-5 py-8 border-r border-gray-100">
+                <div class="text-center">
+                    <div class="text-2xl font-semibold">12</div>
+                    <div class="text-gray-500">Votes</div>
+                </div>
+                <div class="mt-8">
+                    <button
+                        class="w-20 px-4 py-3 font-bold uppercase transition duration-150 ease-in bg-gray-200 border text-xxs hover:border-gray-400 rounded-xl">
+                        Voter</button>
+                </div>
+            </div>
+            <div class="flex px-2 py-6">
+                <a href="#" class="flex-none">
+                    <img src="https://i.pravatar.cc/150?u=a042581f4e29026704d" alt="avatar"
+                        class="w-14 h-14 rounded-xl">
+                </a>
+                <div class="mx-4">
+                    <h4 class="text-xl font-semibold">
+                        <a href="#" class="hover:underline">Lorem ipsum dolor sit amet </a>
+                    </h4>
+                    <div class="mt-3 text-gray-600 line-clamp-3">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, autem, aliquid quae quisquam
+                        consequuntur a earum quos quam fugit voluptate voluptatem necessitatibus quod adipisci
+                        assumenda. Nisi eveniet illo officiis quidem nulla, commodi sunt eaque id maxime quis blanditiis
+                        quisquam velit, fuga rerum nostrum mollitia laboriosam eos exercitationem, tenetur possimus
+                        error ipsa deserunt. Maxime neque doloremque vitae sed provident saepe a nihil enim soluta,
+                        rerum natus molestias, facilis asperiores delectus reiciendis quibusdam illo iure, ea quos
+                        quaerat. Vel nesciunt illo eius. Quasi laudantium commodi totam, distinctio fugit veritatis.
+                        Quia nemo similique culpa dolorem, eveniet sed perspiciatis nisi voluptatibus officia, expedita
+                        veritatis!
+                    </div>
+                    <div class="flex items-center justify-between mt-6">
+                        <div class="flex items-center space-x-2 text-xs font-semibold text-gray-400">
+                            <div class="">il y a 10h</div>
+                            <div class="">&bull;</div>
+                            <div class="">Category 1</div>
+                            <div class="">&bull;</div>
+                            <div class="">3 commentaires</div>
+                        </div>
+                        <div class="flex items-center space-x-2">
+                            <div
+                                class="px-4 py-2 font-bold leading-none text-center uppercase bg-gray-200 rounded-full text-xxs w-28 h-7">
+                                Ouvrir
+                            </div>
+                            <button
+                                class="relative px-4 py-2 transition duration-150 ease-in bg-gray-100 rounded-full hover:bg-gray-200 h-7">&bull;&bull;&bull;
+
+                                <ul
+                                    class="absolute w-64 py-3 ml-8 font-semibold text-left bg-white shadow-dialog rounded-xl">
+                                    <li>
+                                        <a href="#"
+                                            class="block px-5 py-3 transition duration-150 ease-in hover:bg-gray-100">
+                                            Marquer comme spam
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#"
+                                            class="block px-5 py-3 transition duration-150 ease-in hover:bg-gray-100">
+                                            Supprimer post
+                                        </a>
+                                    </li>
+                                </ul>
+                            </button>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div><!-- fin idea-container -->
+        </div>
+
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,9 +28,8 @@
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
 
-                            <a href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                                                                                                                                                 this.closest('form').submit();">
+                            <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                        this.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </a>
                         </form>
